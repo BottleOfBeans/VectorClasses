6a5b38922dc7202e8efd5c5f09cc0d656f9b3fae
Romir <romir.tandon@gmail.com>
2023-03-16 23:21:50 -0400

    I made this cool looking 2D vector!

--- src/Vector2.java
+++ src/Vector2.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Vector2 {
+public class Vector2 {
+    public double x;
+    public double y;
+
+    public Vector2(double gx, double gy){
+            x = gx;
+            y = gy;
+    }
+    public Vector2 normalize(){
+        return new Vector2(x/this.magnitude(), y/this.magnitude());
+    }
+    public double magnitude(){
+        return Math.sqrt(x*x + y*y);
+    }
+    public double sqrMagnitude(){
+        return (x*x + y*y);
+    }
 }
